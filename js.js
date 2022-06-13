ddbc0a7e80ef4349c23a158a7b957ab821f6aea1
Erekle99 <erekledateshidze99@gmail.com>
2022-06-14 00:42:20 +0400

    1
    1

--- js.js
+++ js.js
@@ -1,17 +1,13 @@
 function increaseValue() {
-
-    const value = parseInt(document.getElementById('number').value, 10);
+    value = parseInt(document.getElementById('number').value);
     value = isNaN(value) ? 0 : value;
     value++;
-    document.getElementById("number".value) = value;
+    document.getElementById('number').value = value;
 }
 
-
-
 function decreaseValue() {
-
-    const value = parseInt(document.getElementById('number').value, 10);
+    value = parseInt(document.getElementById('number').value);
     value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById("number".value) = value;
+    value--;
+    document.getElementById('number').value = value;
 }
